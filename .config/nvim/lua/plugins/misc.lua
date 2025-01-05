ca4038a586ec84e3bcdfda50550423a97f75375f
Yuta Tokoi <yutatokoi@proton.me>
2025-01-06 08:47:19 +0900

    add neovim configuration files

---
+++ .config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,42 @@
+-- Standalone plugins with less than 10 lines of config go here
+return {
+  {
+    -- Hints keybinds
+    'folke/which-key.nvim',
+    opts = {
+      -- win = {
+      --   border = {
+      --     { '┌', 'FloatBorder' },
+      --     { '─', 'FloatBorder' },
+      --     { '┐', 'FloatBorder' },
+      --     { '│', 'FloatBorder' },
+      --     { '┘', 'FloatBorder' },
+      --     { '─', 'FloatBorder' },
+      --     { '└', 'FloatBorder' },
+      --     { '│', 'FloatBorder' },
+      --   },
+      -- },
+    },
+  },
+  {
+    -- Autoclose parentheses, brackets, quotes, etc.
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    opts = {},
+  },
+  {
+    -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  {
+    -- high-performance color highlighter
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+}
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-Activating `.zshrc` aliases:
+# Dotfiles
+
+## Activating `.zshrc` aliases:
 
 ```
 source ~/.zshrc
 ```
+
+## Files and directories
+
+- `settings.json` is the settings file for VSCode/VSCodium
+- `.config` includes the configuration settings for Neovim
