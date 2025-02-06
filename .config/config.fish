7e551690550576fb96237e3a5da7260d9276176d
Yuta Tokoi <yutatokoi@proton.me>
2025-02-07 07:08:07 +0900

    it smells fishy

---
+++ .config/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+# Preferred editor for local and remote sessions
+if test -n "$SSH_CONNECTION"
+    set -x EDITOR 'vim'
+else
+    set -x EDITOR 'nvim'
+end
+
+# Navigate man pages with neovim
+set -x MANPAGER 'nvim +Man!'
+
+# Aliases
+alias dl="cd ~/Downloads"
+alias do="cd ~/Documents"
+alias pd="cd $HOME/Library/CloudStorage/ProtonDrive-yutatokoi@proton.me-folder"
+
+starship init fish | source
