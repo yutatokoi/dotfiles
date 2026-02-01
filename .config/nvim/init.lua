23235a6137e98c1e350153dd097421720b077152
Yuta Tokoi <yutatokoi@proton.me>
2026-02-01 15:18:22 +0900

    feat: add neovim-lspconfig with gopls and lua_ls. Only supported for >v0.12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 require('options')
 require('keymaps')
+
+vim.opt.confirm = true
+vim.opt.signcolumn = "yes:1"
+
+vim.pack.add { 'https://github.com/neovim/nvim-lspconfig' }
+vim.lsp.enable('gopls')
+vim.lsp.enable('lua_ls')
